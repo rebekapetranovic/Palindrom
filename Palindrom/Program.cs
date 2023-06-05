cba9da845b3efbcb009ad83e56f7a27cd0e706b0
Rebeka <Rebeka@BECCA>
2023-06-05 14:58:20 +0200

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Prvi
+{
+    class Program
+    {
+        /// <summary>
+        /// ISPITATI ZA SVAKU RIJEC U RECENICI JE LI PALINDRON
+        /// </summary>
+        static void Main()
+        {
+            int i;
+            int n = 0;
+            string rec;
+            Console.Write("Upišite rečenicu: ");
+            rec = Console.ReadLine();
+            string[] rij = rec.Split();
+            string novo = null;
+            foreach (string x in rij)
+            {
+                n = x.Length;
+                for (i = n - 1; i >= 0; i--)
+                {
+                    novo += x[i];
+                }
+                if (novo == x)
+                {
+                    Console.WriteLine("Rijec {0} je palindrom", x);
+                }
+                else
+                {
+                    Console.WriteLine("Rijec {0} nije palindrom", x);
+                }
+            }
+        }
+    }
+}
